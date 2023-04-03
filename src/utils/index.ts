4c6341d80eb7e4c2dbd95ee14d9cb6f5d44abdb2
lim6112j <lim6112j@gmail.com>
2023-04-04 01:57:00 +0900

    not-null assertion operator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,11 @@
-const log = (function () {
-  return function (msg: string) {
-    return function (v: any) {
-      const desc = msg + " => " + v;
-      console.log(desc);
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(desc));
-      document.getElementById("results")!.appendChild(li);
-    };
+const log = function (msg: string) {
+  return function (v: any) {
+    const desc = msg + " => " + v;
+    console.log(desc);
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(desc));
+    document.getElementById("results")?.appendChild(li) ?? false;
   };
-})();
+};
 
 export { log };
